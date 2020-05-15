11e43be931d7bbcfc0d4dd75685ee8ee9bb2456d
Malasia Woods <55638544+malasia-415@users.noreply.github.com>
2020-05-15 12:28:23 -0700

    created memory class

---
+++ memory.js
@@ -0,0 +1,62 @@
+class Memory {
+  constructor() {
+    this.memory = new Float64Array(1024);
+    this.head = 0;
+  }
+
+  allocate(size) {
+    if (this.head + size > this.memory.length) {
+      return null;
+    }
+
+    let start = this.head;
+
+    this.head += size;
+    return start;
+  }
+
+  free(ptr) {}
+
+  copy(toIdx, fromIdx, size) {
+    if (fromIdx === toIdx) {
+      return;
+    }
+    
+class Array {
+    constructor() {
+        this.length = 0;
+        this.ptr = memory.allocate(this.length);
+    }
+    
+    if (fromIdx > toIdx) {
+      // Iterate forwards
+      for (let i = 0; i < size; i++) {
+        this.set(toIdx + i, this.get(fromIdx + i));
+      }
+    } else {
+      // Iterate backwards
+      for (let i = size - 1; i >= 0; i--) {
+        this.set(toIdx + i, this.get(fromIdx + i));
+      }
+    }
+  }
+  _resize(size) {
+        const oldPtr = this.ptr;
+        this.ptr = memory.allocate(size);
+        if (this.ptr === null) {
+            throw new Error('Out of memory');
+        }
+        memory.copy(this.ptr, oldPtr, this.length);
+        memory.free(oldPtr);
+    }
+
+  get(ptr) {
+    return this.memory[ptr];
+  }
+
+  set(ptr, value) {
+    this.memory[ptr] = value;
+  }
+}
+
+module.exports = Memory;
